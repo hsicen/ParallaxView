4ce3971f626da51848765afd419c346fa46a4b82
huangsc4 <huangsc4@lenovo.com>
2024-01-17 10:06:31 +0800

    feat(基础架构)：项目初始化。

---
+++ app/src/main/java/com/hsicen/parallaxview/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.hsicen.parallaxview
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hsicen.parallaxview.ui.theme.ParallaxViewTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      ParallaxViewTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+          Greeting("Android", modifier = Modifier.clickable {
+            SampleList1.start(this)
+          })
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+  Text(
+    text = "Hello $name!",
+    modifier = modifier
+  )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  ParallaxViewTheme {
+    Greeting("Android")
+  }
+}
