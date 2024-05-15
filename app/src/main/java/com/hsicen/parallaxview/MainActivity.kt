4465c1aace84675fe8005898bbf431266d9f24b6
huangsc4 <huangsc4@lenovo.com>
2024-05-15 14:38:08 +0800

    feat(功能实现)：添加循环接口和方向控制接口。

--- app/src/main/java/com/hsicen/parallaxview/MainActivity.kt
+++ app/src/main/java/com/hsicen/parallaxview/MainActivity.kt
@@ -3,14 +3,18 @@ package com.hsicen.parallaxview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hsicen.parallaxview.ui.theme.ParallaxViewTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,10 +22,11 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       ParallaxViewTheme {
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android", modifier = Modifier.clickable {
-            SampleList2.start(this)
-          })
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.background
+        ) {
+          MainContent()
         }
       }
     }
@@ -34,6 +39,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     text = "Hello $name!",
     modifier = modifier
   )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainContent() {
+  val current = LocalContext.current
+
+  Column {
+    OutlinedButton(onClick = {
+      SampleList2.start(current as ComponentActivity)
+    }, modifier = Modifier.padding(16.dp)) {
+      Text(
+        text = "ParallaxView",
+        modifier = Modifier.padding(16.dp)
+      )
+    }
+
+    OutlinedButton(onClick = {
+      SampleList1.start(current as ComponentActivity)
+    }, modifier = Modifier.padding(16.dp)) {
+      Text(
+        text = "ImmersiveList",
+        modifier = Modifier.padding(16.dp)
+      )
+    }
+  }
 }
 
 @Preview(showBackground = true)
--- parallaxview/src/main/java/com/hsicen/parallaxview/ParallaxDirection.kt
+++ parallaxview/src/main/java/com/hsicen/parallaxview/ParallaxDirection.kt
@@ -1,1 +1,1 @@
-package com.hsicen.parallaxviewsealed class ParallaxDirection(val value: Int) {  data object Forward : ParallaxDirection(1)  data object Reverse : ParallaxDirection(-1)}
+package com.hsicen.parallaxviewsealed class ParallaxDirection(val value: Int) {  data object Forward : ParallaxDirection(1)  data object Backward : ParallaxDirection(-1)}
